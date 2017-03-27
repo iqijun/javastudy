9e1d891eff112838d0193511a75056dcbf2f864b
骐骏 <xgs2168@foxmail.com>
2017-03-27 16:16:10 +0800

    单例模式

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
     一、 国美面试题，三个线程依次读取字符串中的n个字符，并输出
          1. printThreadTest.java
          2. ReadString.java
+
+# 设计模式
+    一、单例模式
+        1.（饿汉模式）[\src\main\java\com\fullstacker\study\designpattern\singleton\HungrySingleton.java]
+        2.懒汉模式
+        3.双重检验方式（懒汉模式）
---
+++ src/main/java/com/fullstacker/study/designpattern/singleton/DubbleSingleton.java
@@ -0,0 +1,44 @@
+package com.fullstacker.study.designpattern.singleton;
+
+/**
+ * 双重判断法
+ *
+ * @author xingguishuai
+ * @create 2017-03-27-16:06
+ **/
+public class DubbleSingleton {
+    private static DubbleSingleton intance ;
+
+    private DubbleSingleton(){
+
+    }
+    /**
+     * <p>功能描述：判断是否存在存在实例，如果不存在则，创建对象
+     * 此时，否则返回实例，为线程不安全</p>
+     * @return
+     * @param
+     * @author xingguishuai
+     * @Date 2017-03-27 15:59
+     * @since 1.0
+     */
+    public static DubbleSingleton getIntance(){
+        //第一次检验是否为null
+        if(null == intance){
+            try {
+                //模拟生成对象前准备
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //同步代码块
+            synchronized (DubbleSingleton.class){
+                //创建对象前，再次校验是否为null
+                if(null == intance){
+                    intance = new DubbleSingleton();
+                }
+            }
+        }
+        return intance;
+    }
+
+}
