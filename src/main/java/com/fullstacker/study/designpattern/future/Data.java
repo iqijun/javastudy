79d8d4e8be9ef70fcef9b1a39c88d6aebb1a4e7c
骐骏 <xgs2168@foxmail.com>
2017-04-11 15:58:10 +0800

    Future模式示例

---
+++ src/main/java/com/fullstacker/study/designpattern/future/Data.java
@@ -0,0 +1,7 @@
+package com.fullstacker.study.designpattern.future;
+
+public interface Data {
+
+	String getRequest();
+
+}
---
+++ src/test/java/com/fullstacker/study/test.java
@@ -0,0 +1,21 @@
+package com.fullstacker.study;
+
+import static javafx.scene.input.KeyCode.J;
+
+/**
+ * @author xingguishuai
+ * @create 2017-03-31-18:17
+ **/
+public class test {
+    /**
+    * <p>功能描述：</p>
+    * @return
+    * @param
+    * @author xingguishuai
+    * @Date 2017-03-31 18:21
+    * @since 1.0
+    */
+    public static void main(String[] args){
+
+    }
+}
