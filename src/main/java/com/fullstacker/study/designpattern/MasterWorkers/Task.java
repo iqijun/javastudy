22cde8eb57a9de632b27006419b9f38adc085db9
骐骏 <xgs2168@foxmail.com>
2017-04-13 18:58:18 +0800

    master-worker模式

---
+++ src/main/java/com/fullstacker/study/designpattern/MasterWorkers/Task.java
@@ -0,0 +1,13 @@
+package com.fullstacker.study.designpattern.MasterWorkers;
+
+/**
+ * task类
+ *
+ * @author xingguishuai
+ * @create 2017-04-13-17:27
+ **/
+public interface Task {
+
+    public abstract Object doTask();
+
+}
---
+++ src/main/java/com/fullstacker/study/designpattern/MasterWorkers/example/GradeTask.java
@@ -0,0 +1,53 @@
+package com.fullstacker.study.designpattern.MasterWorkers.example;
+
+import com.fullstacker.study.designpattern.MasterWorkers.Task;
+
+/**
+ * 计算学生成绩任务
+ *
+ * @author xingguishuai
+ * @create 2017-04-13-18:30
+ **/
+public class GradeTask implements Task {
+    /**
+     * 学生ID
+     */
+    private Long sutudentId;
+    /**
+     * 数学成绩
+     */
+    private float mathGrade;
+    /**
+     * 语文成绩
+     */
+    private float chineseGrade;
+
+    public Long getSutudentId() {
+        return sutudentId;
+    }
+
+    public void setSutudentId(Long sutudentId) {
+        this.sutudentId = sutudentId;
+    }
+
+    public float getMathGrade() {
+        return mathGrade;
+    }
+
+    public void setMathGrade(float mathGrade) {
+        this.mathGrade = mathGrade;
+    }
+
+    public float getChineseGrade() {
+        return chineseGrade;
+    }
+
+    public void setChineseGrade(float chineseGrade) {
+        this.chineseGrade = chineseGrade;
+    }
+
+    @Override
+    public Object doTask() {
+        return chineseGrade + mathGrade;
+    }
+}
