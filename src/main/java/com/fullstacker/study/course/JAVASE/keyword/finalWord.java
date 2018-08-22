5546f7312ca9a4a61f644f6b839ad6bf9ded582e
骐骏 <xgs2168@foxmail.com>
2018-08-22 23:28:52 +0800

    final常量

--- src/main/java/com/fullstacker/study/course/JAVASE/keyword/finalWord.java
+++ src/main/java/com/fullstacker/study/course/JAVASE/keyword/finalWord.java
@@ -4,8 +4,14 @@ public class finalWord {
 
     public static final void main(String[] args){
 
-        final String s1 = "finalStr";
-
-
+        String s1 = "finalStr";
+        final String s2 = "final";
+        String s3 = "final";
+        String s2Join = s2 + "Str";
+        String s3Join = s3 + "Str";
+        System.out.println("s2Join:"+s2Join);
+        System.out.println("s3Join:"+s3Join);
+        System.out.println(s1 == s2Join);
+        System.out.println(s1 == s3Join);
     }
 }
