f7d233e85359c74dfc52030a4a1e0019b9ec7edd
骐骏 <xgs2168@foxmail.com>
2019-05-20 23:46:10 +0800

    原子引用，带stamp的原子引用

---
+++ src/main/java/com/fullstacker/study/course/concurrent/AtomicStampedReferenceDemo.java
@@ -0,0 +1,66 @@
+package com.fullstacker.study.course.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+public class AtomicStampedReferenceDemo {
+
+    static User user1 = new User("zhangsan",10);
+
+     static AtomicStampedReference atomicStampedReference = new AtomicStampedReference(user1,1);
+
+    public static  void main(String[] args){
+
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName()+"获得版本号："+atomicStampedReference.getStamp());
+
+                try {
+//                    睡1秒保证线程T2可以获取到相同的版本号
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                User lisi = new User("lisi", 12);
+
+                atomicStampedReference.compareAndSet(user1,lisi,atomicStampedReference.getStamp(),atomicStampedReference.getStamp()+ 1);
+                atomicStampedReference.compareAndSet(lisi,user1,atomicStampedReference.getStamp(),atomicStampedReference.getStamp()+ 1);
+
+                System.out.println(Thread.currentThread().getName()+"修改完成，" +
+                        "最终值为："+atomicStampedReference.get(new int[]{atomicStampedReference.getStamp()})
+                        +"版本号为"+atomicStampedReference.getStamp());
+            }
+        },"t1").start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName()+"获得版本号："+atomicStampedReference.getStamp());
+
+                try {
+//                    睡1秒保证线程T1可以完成1个ABA修改
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                User wangwu = new User("wangwu", 12);
+
+                boolean isSuccess = atomicStampedReference.compareAndSet(user1,wangwu, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+
+
+                System.out.println(Thread.currentThread().getName()+"修改完成，修改结果为" +isSuccess+
+                        "最终值为："+atomicStampedReference.get(new int[]{atomicStampedReference.getStamp()})
+                        +"版本号为"+atomicStampedReference.getStamp());
+            }
+        },"t2").start();
+
+    }
+
+}
